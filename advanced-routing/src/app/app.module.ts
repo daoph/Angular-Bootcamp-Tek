c9a6c5fada19f4d6583645222ee949328062fa80
daoph <30603149+daoph@users.noreply.github.com>
2017-11-09 13:18:08 -0500

    advanced-routing

---
+++ advanced-routing/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { AdminComponent } from './admin/admin.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { RouteErrorComponent } from './error/route.error.component';
+import { AuthService } from './auth.service';
+import { ProductsModule } from './products/products.module';
+
+
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    ProductsModule,
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    AdminComponent,
+    RouteErrorComponent,
+  ],
+
+  providers: [ AuthService ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
